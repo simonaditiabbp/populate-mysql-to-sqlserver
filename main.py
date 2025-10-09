4811d4485b04ded25ef9643cea27248592322792
simonaditiabbp <simon.aditia@borneo.co.id>
2025-10-09 17:58:03 +0700

    update logic get log (status), update log, add heartbeat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 from dotenv import load_dotenv
+import requests
+import threading
 import time
 import datetime
 import mysql.connector
@@ -28,7 +30,14 @@ SQLSRV_TABLE = os.getenv("SQLSRV_TABLE")
 WB_TAG = os.getenv("WB_TAG", "DEFAULT_WB")
 SYNC_INTERVAL = int(os.getenv("SYNC_INTERVAL", 10))
 
-
+def send_heartbeat(pc_name):
+    while True:
+        try:
+            requests.post("http://127.0.0.1:5000/api/heartbeat", json={"pc_name": pc_name}, timeout=3)
+        except Exception as e:
+            print("⚠️ Gagal kirim heartbeat:", e)
+        time.sleep(10)
+        
 # === Fungsi bantu ===
 def get_shift_date(dt):
     """Mengembalikan tanggal shift berdasarkan jam kerja."""
@@ -64,7 +73,7 @@ def sync_data():
     mysql_cur = mysql_conn.cursor(dictionary=True)
 
     try:
-        mysql_cur.execute(f"SELECT NOURUT1, aksi, PLANT_ID FROM {MYSQL_LOG} ORDER BY log_time")
+        mysql_cur.execute(f"SELECT NOURUT1, AKSI, PLANT_ID FROM {MYSQL_LOG} WHERE STATUS = 'PENDING' ORDER BY log_time")
         logs = mysql_cur.fetchall()
         if not logs:
             print("Tidak ada log baru di", MYSQL_LOG)
@@ -75,10 +84,21 @@ def sync_data():
         for entry in logs:
             NOURUT1 = entry.get('NOURUT1')
             PLANT_ID = entry.get('PLANT_ID')
-            aksi = (entry.get('aksi') or 'UPDATE').upper()
+            aksi = (entry.get('AKSI') or 'UPDATE').upper()
 
-            try:
-                if aksi in ('INSERT', 'UPDATE'):
+            try:                                
+                mysql_cur.execute(
+                    f"SELECT * FROM {MYSQL_LOG} WHERE NOURUT1 = %s AND PLANT_ID = %s order by COUNTER_DONE desc LIMIT 1",
+                    (NOURUT1, PLANT_ID)
+                )
+                row_counter_done = mysql_cur.fetchone()
+                row_counter_done_old = row_counter_done['COUNTER_DONE']                                                 
+                row_counter_done_update = row_counter_done_old + 1
+                
+                print("row_counter_done_old: ", row_counter_done_old)
+                print("row_counter_done_update: ", row_counter_done_update)
+                                
+                if aksi in ('INSERT', 'UPDATE'):                    
                     # ambil row dari MySQL
                     mysql_cur.execute(
                         f"SELECT * FROM {MYSQL_TABLE} WHERE NOURUT1 = %s AND PLANT_ID = %s",
@@ -87,20 +107,22 @@ def sync_data():
                     row = mysql_cur.fetchone()
 
                     if not row:
-                        print(f"Baris {NOURUT1}-{PLANT_ID} tidak ditemukan di MySQL (skip).")
+                        error_notfound_mysql = f"Baris {NOURUT1}-{PLANT_ID} tidak ditemukan di MySQL (skip)."
+                        print(error_notfound_mysql)
+                        
                         mysql_cur.execute(
-                            f"DELETE FROM {MYSQL_LOG} WHERE NOURUT1 = %s AND PLANT_ID = %s",
-                            (NOURUT1, PLANT_ID)
+                            f"UPDATE {MYSQL_LOG} SET STATUS = 'FAILED', MESSAGE = %s WHERE NOURUT1 = %s AND PLANT_ID = %s",
+                            (error_notfound_mysql, NOURUT1, PLANT_ID)
                         )
                         mysql_conn.commit()
                         continue
-
+                    
                     # Tambahkan kolom buatan
                     tanggal_shift = get_shift_date(row.get("TANGGAL2"))
-                    row["tanggal_shift"] = tanggal_shift
-                    row["date_sync"] = datetime.datetime.now()
-                    row["wb_tag"] = WB_TAG
-                    row["deleted"] = 0
+                    row["TANGGAL_SHIFT"] = tanggal_shift
+                    row["DATE_SYNC"] = datetime.datetime.now()
+                    row["WB_TAG"] = WB_TAG
+                    row["DELETED"] = 0
 
                     col_names = list(row.keys())
 
@@ -117,7 +139,7 @@ def sync_data():
                         old_dict = dict(zip(columns, old_row))
 
                         # cari kolom yang berubah (selain PK & manual field)
-                        skip_fields = ('NOURUT1', 'PLANT_ID', 'deleted', 'date_sync', 'wb_tag')
+                        skip_fields = ('NOURUT1', 'PLANT_ID', 'DELETED', 'DATE_SYNC', 'WB_TAG')
                         changed_cols = [
                             c for c in col_names
                             if c not in skip_fields and str(row[c]) != str(old_dict.get(c))
@@ -128,10 +150,10 @@ def sync_data():
                             params = [row[c] for c in changed_cols] + [row['NOURUT1'], row['PLANT_ID']]
                             update_sql = f"""
                                 UPDATE {SQLSRV_TABLE}
-                                SET {set_clause}, [date_sync] = ?
+                                SET {set_clause}, [DATE_SYNC] = ?
                                 WHERE NOURUT1 = ? AND PLANT_ID = ?
                             """
-                            params = [row[c] for c in changed_cols] + [row["date_sync"], row['NOURUT1'], row['PLANT_ID']]
+                            params = [row[c] for c in changed_cols] + [row["DATE_SYNC"], row['NOURUT1'], row['PLANT_ID']]
                             sqlsrv_cur.execute(update_sql, params)
                             sqlsrv_conn.commit()
                             print(f"UPDATE parsial ({len(changed_cols)} kolom): {NOURUT1}-{PLANT_ID}")
@@ -139,9 +161,15 @@ def sync_data():
                             print(f"Tidak ada perubahan untuk {NOURUT1}-{PLANT_ID}.")
                         
                         # hapus log
+                        # mysql_cur.execute(
+                        #     f"DELETE FROM {MYSQL_LOG} WHERE NOURUT1 = %s AND PLANT_ID = %s",
+                        #     (NOURUT1, PLANT_ID)
+                        # )
+                        # mysql_conn.commit()
+                        
                         mysql_cur.execute(
-                            f"DELETE FROM {MYSQL_LOG} WHERE NOURUT1 = %s AND PLANT_ID = %s",
-                            (NOURUT1, PLANT_ID)
+                            f"UPDATE {MYSQL_LOG} SET STATUS = 'SUCCESS', MESSAGE = 'Data updated successfully', COUNTER_DONE = %s WHERE NOURUT1 = %s AND PLANT_ID = %s AND AKSI = 'UPDATE' AND COUNTER_DONE = %s",
+                            (row_counter_done_update, NOURUT1, PLANT_ID, 0)
                         )
                         mysql_conn.commit()
 
@@ -156,11 +184,18 @@ def sync_data():
                             sqlsrv_cur.execute(insert_sql, params)
                             sqlsrv_conn.commit()
                             print(f"INSERT sukses: {NOURUT1}-{PLANT_ID}")
+                            # mysql_cur.execute(
+                            #     f"DELETE FROM {MYSQL_LOG} WHERE NOURUT1 = %s AND PLANT_ID = %s",
+                            #     (NOURUT1, PLANT_ID)
+                            # )
+                            # mysql_conn.commit()
+                            
                             mysql_cur.execute(
-                                f"DELETE FROM {MYSQL_LOG} WHERE NOURUT1 = %s AND PLANT_ID = %s",
-                                (NOURUT1, PLANT_ID)
+                                f"UPDATE {MYSQL_LOG} SET STATUS = 'SUCCESS', MESSAGE = 'Data inserted successfully', COUNTER_DONE = %s WHERE NOURUT1 = %s AND PLANT_ID = %s AND AKSI = 'INSERT' AND COUNTER_DONE = %s",
+                                (row_counter_done_update, NOURUT1, PLANT_ID, 0)
                             )
                             mysql_conn.commit()
+                            
                         except Exception as e:
                             print(f"Gagal INSERT {NOURUT1}-{PLANT_ID}: {e}")
                             continue
@@ -172,11 +207,20 @@ def sync_data():
                             (NOURUT1, PLANT_ID)
                         )
                         sqlsrv_conn.commit()
+                        # mysql_cur.execute(
+                        #     f"DELETE FROM {MYSQL_LOG} WHERE NOURUT1 = %s AND PLANT_ID = %s",
+                        #     (NOURUT1, PLANT_ID)
+                        # )
+                        # mysql_conn.commit()
+                        
+                        print("#DELETE row_counter_done_update: ", row_counter_done_update)
+                        print("#DELETE row_counter_done_old: ", row_counter_done_old)
                         mysql_cur.execute(
-                            f"DELETE FROM {MYSQL_LOG} WHERE NOURUT1 = %s AND PLANT_ID = %s",
-                            (NOURUT1, PLANT_ID)
+                            f"UPDATE {MYSQL_LOG} SET STATUS = 'SUCCESS', MESSAGE = 'Data deleted successfully', COUNTER_DONE = %s WHERE NOURUT1 = %s AND PLANT_ID = %s AND AKSI = 'DELETE' AND COUNTER_DONE = %s",
+                            (row_counter_done_update, NOURUT1, PLANT_ID, 0)
                         )
                         mysql_conn.commit()
+                        
                         print(f"DELETE flag sukses: {NOURUT1}-{PLANT_ID}")
                     except Exception as e:
                         print(f"Gagal update deleted flag {NOURUT1}-{PLANT_ID}: {e}")
@@ -191,7 +235,14 @@ def sync_data():
                     mysql_conn.commit()
 
             except Exception as e:
-                print(f"ERROR processing {NOURUT1}-{PLANT_ID}: {e}")
+                error_message = f"ERROR processing {NOURUT1}-{PLANT_ID}: {e}"
+                print(error_message)
+                mysql_cur.execute(
+                    f"UPDATE {MYSQL_LOG} SET MESSAGE = %s WHERE NOURUT1 = %s AND PLANT_ID = %s AND COUNTER_DONE = %s",
+                    (error_message, NOURUT1, PLANT_ID, 0)
+                )
+                mysql_conn.commit()
+                
                 continue
 
         print("=== Sinkronisasi selesai ===")
@@ -213,6 +264,8 @@ def sync_data():
 if __name__ == "__main__":
     while True:
         try:
+            PC_NAME = os.getenv("PC_NAME")
+            threading.Thread(target=send_heartbeat, args=(PC_NAME,), daemon=True).start()
             sync_data()
         except Exception as e:
             print("Terjadi error utama:", e)
